d609d727ee9800925f71d1d78c517ffcd0592fa0
Dawid Kolano <dawid.kolano@gmail.com>
2024-06-27 22:06:35 +0200

    Initial commit

---
+++ src/test/java/pl/dk/libraryapp/LibraryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dk.libraryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
